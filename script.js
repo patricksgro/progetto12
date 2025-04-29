bfcec274679ca4c2612962cddbbfb64bb9371467
patricksgro <patricksgro02@gmail.com>
2025-04-29 18:55:20 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function rimuoviLink() {
+    let removeLink = document.getElementById('twitter')
+    removeLink.style.display = 'none'
+}
+
+function hideFather() {
+    let parent = document.querySelector('.titleLeft')
+    parent.remove()
+}
+
+
+function infoAutore() {
+    alert('Marco')
+}
+
+
